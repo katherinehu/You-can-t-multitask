fe070851f5547ff81bc281226b3b879f1590cb62
Patrick Meng <patm314@gmail.com>
2020-11-26 15:12:26 -0500

    created new activities

---
+++ app/src/main/java/com/wave/sbauction/AuctionMainMenu.java
@@ -0,0 +1,14 @@
+package com.wave.sbauction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuctionMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auction_main_menu);
+    }
+}
---
+++ app/src/main/java/com/wave/sbauction/MultiTaskMainMenu.java
@@ -0,0 +1,14 @@
+package com.wave.sbauction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiTaskMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_task_main_menu);
+    }
+}
