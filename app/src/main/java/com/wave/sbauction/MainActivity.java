d4eb06701f70818b386ceb8ccf3914b883fcc6b6
Patrick Meng <patm314@gmail.com>
2020-11-26 15:28:34 -0500

    Created Main Menu

--- app/src/main/java/com/wave/sbauction/MainActivity.java
+++ app/src/main/java/com/wave/sbauction/MainActivity.java
@@ -2,13 +2,48 @@ package com.wave.sbauction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+/*
+Main menu where so you can either go to the multitask game or you can go to the auction
+Fairly simple, not much to see here.
+-P
+ */
+public class MainActivity extends AppCompatActivity{
+
+    Button btnMultitask, btnAuction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Main Menu");
+
+        //regionAssign the button views
+        btnMultitask = findViewById(R.id.btnGame);
+        btnAuction = findViewById(R.id.btnAuctionMenu);
+        //endregion
+
+        //regionOpen the right activities when buttons clicked
+        final Intent openGame = new Intent(this,MultiTaskMainMenu.class);
+        btnMultitask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(openGame);
+            }
+        });
+
+        final Intent openAuction = new Intent(this,AuctionMainMenu.class);
+        btnAuction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(openAuction);
+            }
+        });
+        //endregion
     }
 }
