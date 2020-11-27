29baa4426c7180bc581d4b544f58e04e1629c965
Patrick Meng <patm314@gmail.com>
2020-11-27 14:30:22 -0500

    added checkboxes to turn off games

--- app/src/main/java/com/wave/sbauction/MultiTaskGame.java
+++ app/src/main/java/com/wave/sbauction/MultiTaskGame.java
@@ -3,10 +3,15 @@ package com.wave.sbauction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
@@ -22,8 +27,10 @@ public class MultiTaskGame extends Activity implements SensorEventListener {
     static float zAcceleration = 0;
 
     boolean lostGame = false;
+    static double timeElapsed = 0;
 
     TextView tvTimer;
+    ImageView ivBallGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +38,46 @@ public class MultiTaskGame extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_multi_task_game);
 
         //regionDeclare UI elements
+        tvTimer = findViewById(R.id.tvTimer);
+        ivBallGame = findViewById(R.id.ivBallGame);
+        //endregion
 
         //regionSet up a timer so the user knows how long they've survived. -P
-        this.runOnUiThread(new Runnable() {
+        new Thread(){
             @Override
             public void run() {
                 //Use this to calibrate, since thread.sleep isn't always perfect
                 long firstTime = System.currentTimeMillis();
-
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                while(!lostGame) {
+                    long currentTime = System.currentTimeMillis();
+                    long timeDiff = currentTime - firstTime;
+                    timeElapsed = (double)timeDiff / 1000;
+                    String time = "Time Survived: " + Round(timeElapsed,1);
+                    new updateTimer().execute(time);
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("timeElapsed","Time: " + timeElapsed);
                 }
             }
-        });
+        }.start();
+        //endregion
+
+        //regionFirst game, keep the ball inside the box -P
+        new Thread() {
+            @Override
+            public void run() {
+                Bitmap firstMap = BitmapFactory.decodeResource(getResources(),R.drawable.canvas);
+                Bitmap editableMap = firstMap.copy(Bitmap.Config.ARGB_8888,true);
+                int h = editableMap.getHeight();
+                int w = editableMap.getWidth();
+                while(!lostGame){
+
+                }
+            }
+        }.start();
         //endregion
     }
 
@@ -79,5 +111,19 @@ public class MultiTaskGame extends Activity implements SensorEventListener {
         BigDecimal bd = BigDecimal.valueOf(input);
         bd = bd.setScale(scale, RoundingMode.HALF_UP);
         return bd.doubleValue();
+    }
+
+    //Can't update views
+    class updateTimer extends AsyncTask<String,String,String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            return strings[0];
+        }
+
+        @Override
+        protected void onPostExecute(String time) {
+            super.onPostExecute(time);
+            tvTimer.setText(time);
+        }
     }
 }
