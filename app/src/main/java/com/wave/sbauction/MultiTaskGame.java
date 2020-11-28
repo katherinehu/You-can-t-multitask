1c681caef99407d2021c9fa9088b660ca9c330ab
Patrick Meng <patm314@gmail.com>
2020-11-28 02:21:42 -0500

    oh i'm definitely going to change this, probably implement a real animation because it turns out refreshing imageviews more than twice a second causes problems

--- app/src/main/java/com/wave/sbauction/MultiTaskGame.java
+++ app/src/main/java/com/wave/sbauction/MultiTaskGame.java
@@ -1,26 +1,38 @@
 package com.wave.sbauction;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /*
 Here, we have the entire game, which will prove to the humans that their multitasking ability is nonexistent.
  */
 public class MultiTaskGame extends Activity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor mAccelerometer;
 
     static float xAcceleration = 0;
     static float yAcceleration = 0;
@@ -59,22 +71,94 @@ public class MultiTaskGame extends Activity implements SensorEventListener {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    Log.d("timeElapsed","Time: " + timeElapsed);
                 }
             }
         }.start();
         //endregion
 
         //regionFirst game, keep the ball inside the box -P
+        //Setup accelerometer so game can be controlled
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         new Thread() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void run() {
-                Bitmap firstMap = BitmapFactory.decodeResource(getResources(),R.drawable.canvas);
-                Bitmap editableMap = firstMap.copy(Bitmap.Config.ARGB_8888,true);
-                int h = editableMap.getHeight();
-                int w = editableMap.getWidth();
-                while(!lostGame){
+                //Get an image of what the playing field looks like
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inScaled = false;
+                Bitmap firstMap = BitmapFactory.decodeResource(getResources(),R.drawable.canvas,options);
+                int h = firstMap.getHeight();
+                int w = firstMap.getWidth();
+                int [] canvasPixels = new int[h*w];
+                firstMap.getPixels(canvasPixels,0,w,0,0,w,h);
+                //The color of the background, for reference, stored here
+                int baseColor = canvasPixels[1];
 
+                //Velocity of the ball is stored here, which is used to adjust the position
+                //of the ball until it is no longer on the screen, at which point the player fails
+                //this particular challenge. Starts off at a random value to keep things interesting.
+                double xVelocity = 0;//ThreadLocalRandom.current().nextDouble(-10,10);
+                double yVelocity = 0;//ThreadLocalRandom.current().nextDouble(-10,10);
+                //How much position had changed since the start of the game
+                double xPosition = 0;
+                double yPosition = 0;
+                //Set how fast this game will refresh, also use that time for physics calculations
+                int refreshMilliseconds = 500;
+                //Make all the movements less dramatic so the user actually has time to react, this is adjusted until the game runs reasonably
+                double scalingFactor = 1;
+
+                while(!lostGame){
+                    //Locate where the ball is in the canvas
+                    ArrayList<Integer> ballLocations = new ArrayList<>();
+                    for (int i = 0; i < h * w; ++i) {
+                        if (canvasPixels[i] == Color.RED) {
+                            ballLocations.add(i);
+                            Log.d("ballLocation",Integer.toString(i));
+                            //Set to the base color, so that that the position of the ball can be updated.
+                            canvasPixels[i] = baseColor;
+                        }
+                    }
+                    //Calculate new ball velocity
+//                    xVelocity += yAcceleration*yAcceleration*((double)refreshMilliseconds/1000) / scalingFactor * 0.1;
+//                    yVelocity -= xAcceleration*xAcceleration*((double)refreshMilliseconds/1000) / scalingFactor * 0.1;
+                    //Calculate new ball position
+                    xPosition += yAcceleration*((double)refreshMilliseconds/1000) / scalingFactor;
+                    yPosition += xAcceleration*((double)refreshMilliseconds/1000) / scalingFactor;
+
+                    //Store where the ball will be now.
+                    ArrayList<Integer> newBallLocations = new ArrayList<>();
+                    for(int locations:ballLocations){
+                        //Check to see if it went out the left or right bound.
+                        int currentRow = locations % w;
+                        if ((currentRow + (int)xPosition) > w || (currentRow + (int)xPosition) < 0){
+                            lostGame = true;
+                            break;
+                        }
+                        newBallLocations.add(locations + (int)xPosition + w * (int)yPosition);
+                    }
+
+                    //Try to update the image, if it doesn't work, that means player has failed game
+                    for(int locations:newBallLocations) {
+                        try {
+                            canvasPixels[locations] = Color.RED;
+                        } catch (Exception e) {
+                            //This means out of bounds, and the player has failed the game
+                            lostGame = true;
+                        }
+                    }
+
+                    //If the above worked, update the image
+                    if (!lostGame) {
+                        ivBallGame.setImageBitmap(Bitmap.createBitmap(canvasPixels, w, h, Bitmap.Config.ARGB_8888));
+                    }
+
+                    //Delay a bit so the player has time to react.
+                    try {
+                        Thread.sleep(refreshMilliseconds);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }.start();
@@ -91,6 +175,18 @@ public class MultiTaskGame extends Activity implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     //Method to round numbers to specified level of precision -P
