50ca192d917a0ef6b29d05c19edc91a239ff7d17
Patrick Meng <patm314@gmail.com>
2020-12-16 01:30:37 -0500

    new game added

--- app/src/main/java/com/wave/sbauction/MultiTaskMainMenu.java
+++ app/src/main/java/com/wave/sbauction/MultiTaskMainMenu.java
@@ -10,12 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
 
 public class MultiTaskMainMenu extends AppCompatActivity {
 
     Button btnStartGame;
-    CheckBox checkTilt, checkMath, checkColor, checkTimerReaction, checkBars;
+    CheckBox checkTilt, checkPress, checkColor, checkTimerReaction, checkBars;
 
     /*
     Gives you the option to start the game, and adjust any gameplay settings right on the screen.
@@ -31,7 +30,7 @@ public class MultiTaskMainMenu extends AppCompatActivity {
         //regionDeclare UI elements
         btnStartGame = findViewById(R.id.btnPlayGame);
         checkTilt = findViewById(R.id.checkTilt);
-        checkMath = findViewById(R.id.checkMath);
+        checkPress = findViewById(R.id.checkPress);
         checkColor = findViewById(R.id.checkColor);
         checkTimerReaction = findViewById(R.id.checkTimerReaction);
         checkBars = findViewById(R.id.checkBars);
@@ -51,12 +50,12 @@ public class MultiTaskMainMenu extends AppCompatActivity {
         SharedPreferences data = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         final SharedPreferences.Editor editor = data.edit();
         boolean tiltDisabled = data.getBoolean("tiltDisabled",false);
-        boolean mathDisabled = data.getBoolean("mathDisabled",false);
+        boolean pressDisabled = data.getBoolean("pressDisabled",false);
         boolean colorDisabled = data.getBoolean("colorDisabled",false);
         boolean timerDisabled = data.getBoolean("timerDisabled",false);
         boolean barsDisabled = data.getBoolean("barsDisabled",false);
         checkTilt.setChecked(tiltDisabled);
-        checkMath.setChecked(mathDisabled);
+        checkPress.setChecked(pressDisabled);
         checkTimerReaction.setChecked(timerDisabled);
         checkColor.setChecked(colorDisabled);
         checkBars.setChecked(barsDisabled);
@@ -66,7 +65,7 @@ public class MultiTaskMainMenu extends AppCompatActivity {
         if(tiltDisabled) {
             ++gamesDisabled[0];
         }
-        if(mathDisabled){
+        if(pressDisabled){
             ++gamesDisabled[0];
         }
         if(timerDisabled) {
@@ -88,6 +87,70 @@ public class MultiTaskMainMenu extends AppCompatActivity {
                     ++gamesDisabled[0];
                 } else {
                     editor.putBoolean("tiltDisabled",false);
+                    --gamesDisabled[0];
+                }
+                editor.putInt("gamesDisabled",gamesDisabled[0]);
+                editor.commit();
+            }
+        });
+
+        checkPress.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                //b is true when the button is checked
+                if (b) {
+                    editor.putBoolean("pressDisabled",true);
+                    ++gamesDisabled[0];
+                } else {
+                    editor.putBoolean("pressDisabled",false);
+                    --gamesDisabled[0];
+                }
+                editor.putInt("gamesDisabled",gamesDisabled[0]);
+                editor.commit();
+            }
+        });
+
+        checkTimerReaction.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                //b is true when the button is checked
+                if (b) {
+                    editor.putBoolean("timerDisabled",true);
+                    ++gamesDisabled[0];
+                } else {
+                    editor.putBoolean("timerDisabled",false);
+                    --gamesDisabled[0];
+                }
+                editor.putInt("gamesDisabled",gamesDisabled[0]);
+                editor.commit();
+            }
+        });
+
+        checkColor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                //b is true when the button is checked
+                if (b) {
+                    editor.putBoolean("colorDisabled",true);
+                    ++gamesDisabled[0];
+                } else {
+                    editor.putBoolean("colorDisabled",false);
+                    --gamesDisabled[0];
+                }
+                editor.putInt("gamesDisabled",gamesDisabled[0]);
+                editor.commit();
+            }
+        });
+
+        checkBars.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                //b is true when the button is checked
+                if (b) {
+                    editor.putBoolean("barsDisabled",true);
+                    ++gamesDisabled[0];
+                } else {
+                    editor.putBoolean("barsDisabled",false);
                     --gamesDisabled[0];
                 }
                 editor.putInt("gamesDisabled",gamesDisabled[0]);
